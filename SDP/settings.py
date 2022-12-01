a100e228e5a72a6e4481dfb80358035e6253e92c
RAJGOPAL HOTA <2100032351@KLUNIVERSITY.IN>
2022-12-01 13:31:32 +0530

    Added forgot password and email verify

--- SDP/settings.py
+++ SDP/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iloicx@gvutmpsj4pjtxk+u152ooq)%b25*799c0xd7xo2kf4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
