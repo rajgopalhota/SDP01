dc3ad88a1cac2389323145c07ef0f7a3b7f6f54c
RAJGOPAL HOTA <2100032351@KLUNIVERSITY.IN>
2023-01-01 15:02:23 +0530

    time zone set!

--- SDP/settings.py
+++ SDP/settings.py
@@ -121,11 +121,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
 
+# USE_I18N = True
+
+# USE_TZ = True
+TIME_ZONE =  'Asia/Calcutta'
 USE_I18N = True
-
-USE_TZ = True
+USE_L10N = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
